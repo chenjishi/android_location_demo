bba10a0a41545c4b7aaf9398a8c9ae7326f06ee2
chenjishi <jishi@staff.xianguo.com>
2013-07-19 17:20:38 -0700

    deleted useless files

--- demo/gen/com/example/demo/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.demo;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
